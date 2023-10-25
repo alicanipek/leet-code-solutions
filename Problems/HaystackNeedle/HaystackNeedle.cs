adc0c7c951550d06a40358687f0b2511ae706383
Ali Can Ipek <alican.ipek@outlook.com>
2023-10-25 11:47:19 +0300

    added new solutions

---
+++ Problems/HaystackNeedle/HaystackNeedle.cs
@@ -0,0 +1,15 @@
+
+public class HaystackNeedle {
+    public int StrStr(string haystack, string needle) {
+        int windowSize = needle.Length;
+
+        for(int i = 0; i < haystack.Length - windowSize; i++) {
+            string substr = haystack.Substring(i, windowSize);
+            if(substr == needle) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+}
